737a13dbaf7197c634032374a36a8dcac5ac7691
孙迎迪 <13721418325@163.com>
2023-09-18 15:15:22 +0800

    feature[测试环境变量]

--- src/main/java/com/syd/controller/TestController.java
+++ src/main/java/com/syd/controller/TestController.java
@@ -2,9 +2,9 @@ package com.syd.controller;
 
 import com.syd.service.TestService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -13,12 +13,14 @@ import javax.annotation.Resource;
  * @Description：
  * @created 2023/9/18
  */
-@RestController
-@RequestMapping
+//@RestController
+//@RequestMapping
+@Controller
 public class TestController {
     @Resource
     private TestService testService;
-    @GetMapping("/test")
+
+    // @GetMapping("/test")
     public String test() {
         testService.test();
         return "test";
